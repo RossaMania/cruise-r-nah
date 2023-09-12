228326c19e6c3818b573f783a5a89fe2f4659dc8
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-12 12:37:42 -0500

    Install next-themes. Replace stock favicon with film reel favicon.

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -1,13 +1,13 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "next-themes"
 
 import React from "react";
 
 const Providers = ({ children }) => {
   return (
     <ThemeProvider attribute="class">
-      <div className="dark:bg-gray-700 dark:text-red-200 text-gray-700 bg-slate-100 transition-colors duration-300 min-h-screen select-none">{children}</div>
+      <div className="dark:bg-gray-700 dark:text-red-200 text-gray-700 bg-slate-50 transition-colors duration-300 min-h-screen select-none">{children}</div>
     </ThemeProvider>
   );
 };
