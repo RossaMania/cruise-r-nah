6f0da6db75d6077e60728a19d8830608501ede89
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-11 20:27:40 -0500

    Create DarkModeSwitch functionality. Import useTheme from next-themes. Import useEffect and useState from React. Destructure useTheme into resolvedTheme, theme, and setTheme object. Create mounted var with setMounted state func. Use useEffect to make setMounted true. If not, nothing happens. Conditionally render dark or light mode based on current theme. If system theme available, then it's the resolvedTheme. If not, then currentTheme = theme. setTheme is part of an in-line function with onClick event. Conditionally render light/dark theme. Import icons from react-icons.

--- src/components/DarkModeSwitch.js
+++ src/components/DarkModeSwitch.js
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { MdLightMode } from "react-icons/md";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { useTheme } from "next-themes";
 
 const DarkModeSwitch = () => {
-  return (
-    <div>This will be the switch between Dark Mode and Light.</div>
-  )
-}
+  const { resolvedTheme, theme, setTheme } = useTheme();
 
-export default DarkModeSwitch
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const currentTheme = theme === "system" ? resolvedTheme : theme;
+
+  return (
+    <div>
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdLightMode onClick={() => setTheme("light")} />
+        ) : (
+          <BsFillMoonStarsFill onClick={() => setTheme("dark")} />
+        ))}
+    </div>
+  );
+};
+
+export default DarkModeSwitch;
