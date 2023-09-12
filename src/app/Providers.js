aced832c605723f8a634eb12497227c5bca65aa5
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-11 21:27:33 -0500

    Wrap children in ThemeProvider with div, in Providers file. Style light mode and dark mode themes in that div.

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -5,7 +5,11 @@ import { ThemeProvider } from "next-themes";
 import React from "react";
 
 const Providers = ({ children }) => {
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+  return (
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-gray-700 dark:text-red-200 text-gray-700 bg-slate-100 transition-colors duration-300 min-h-screen select-none">{children}</div>
+    </ThemeProvider>
+  );
 };
 
 export default Providers;
