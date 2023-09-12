7cb2262fd0e9e3fc6e43e76ea2f2fe0ef1a64e1a
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-12 16:42:47 -0500

    Create Tomography route with tomography folder and page.js with Tomography func component and placeholder text.

---
+++ src/app/tomography/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Tomography() {
+  return (
+    <div>
+      <h2>This is the Tomography page.</h2>
+      <p>This page is going to be a data fetch from The Movie Database.</p>
+      <p>The data will be all about Tom Cruise, and will be a good way to test data fetching.</p>
+    </div>
+  );
+}
+
+export default Tomography
