e1a0f7d72e2eb0e5ea2d6898c2caeba4bbe4856f
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-13 13:03:04 -0500

    Create a .env.local file for API key. Import API key as a const to Tomography page.

--- src/app/tomography/page.js
+++ src/app/tomography/page.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
-function Tomography() {
+const API_KEY = process.env.API_KEY
+
+const Tomography = () => {
+
+
   return (
     <div>
       <h2>This is the Tomography page.</h2>
       <p>This page is going to be a data fetch from The Movie Database.</p>
-      <p>The data will be all about Tom Cruise, and will be a good way to test data fetching.</p>
+      <p>
+        The data will be all about Tom Cruise, and will be a good way to test
+        data fetching.
+      </p>
+
+      {/* Here is the url to pull up Tom Cruise movie credits:
+      'https://api.themoviedb.org/3/person/500/movie_credits?language=en-US'; */}
+
+
     </div>
   );
 }
