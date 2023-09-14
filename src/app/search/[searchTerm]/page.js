4872626f20ed44d432aac588ba661f511f4e084d
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-14 17:39:01 -0500

    Create await fetch in SearchPage, with data const for res.json, and results const for results.

--- src/app/search/[searchTerm]/page.js
+++ src/app/search/[searchTerm]/page.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
-const SearchPage = () => {
+const SearchPage = async ({ params }) => {
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US&include_adult=false`
+  );
+
+  if (!res.ok) {
+    throw new Error("Error fetching data!");
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
   return (
     <div>This is the page for search results.</div>
   )
