cb768e3308382ee0b0e391225f47b9aed75ebda4
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-14 16:36:24 -0500

    Create searchbar func component. Render Searchbar component in root page. Remove Searchbar comments from layout.js.

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,6 @@ export default function RootLayout({ children }) {
 
           {/* Navbar */}
 
-          {/* Search */}
           {children}
         </Providers>
       </body>
---
+++ src/components/Searchbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Searchbar = () => {
+  return (
+    <div>This is the search bar.</div>
+  )
+}
+
+export default Searchbar
