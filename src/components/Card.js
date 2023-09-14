160f6e014240043269a9bae9e944fb7b4565c6ad
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-13 22:37:05 -0500

    Style date to be M/D/YYYY format. Round vote average to 0.1 place. Add boolean statements with returns for empty release dates and empty voter averages.

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,37 @@ import { PiShootingStarFill } from "react-icons/pi";
 
 const Card = ({result}) => {
 
+  // Parse the release date string as a Date object
+  let releaseDate = new Date(result.release_date);
+
+  if (result.release_date === "") {
+    formattedReleaseDate = "Release date unknown.";
+  } else {
+
+  // Extract the year, month, and day
+  var year = releaseDate.getFullYear();
+  var month = String(releaseDate.getMonth() + 1); // Month is 0-based, so we add 1
+  var day = String(releaseDate.getDate());
+
+  // Format the date as MM/DD/YYYY
+  var formattedReleaseDate = `${month}/${day}/${year}`;
+  }
+
+
+  const viewerScore = result.vote_average;
+
+  const roundedViewerScore = Math.round(viewerScore * 10) / 10;
+
+
+
   return (
     <div>
       <h2>{result.title}</h2>
+      <p>as {result.character || result.job}</p>
       <p>
-        {result.vote_average} <PiShootingStarFill />
-        {result.release_date}
+        {roundedViewerScore ? roundedViewerScore : "No rating."}
+        <PiShootingStarFill />
+        {formattedReleaseDate}
       </p>
     </div>
   );
