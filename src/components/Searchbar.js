6a5bcaf68be6503dd18649236b391991978b92b5
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-14 17:28:38 -0500

    Style search bar and button to make more visible.

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -20,15 +20,18 @@ const Searchbar = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleSubmit}
+      className="flex max-w-5xl mx-auto justify-between items-center px-4"
+    >
       <input
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         type="text"
         placeholder="Search keywords..."
-        className="w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1"
+        className="w-full h-10 rounded-sm placeholder-slate-100 outline-none bg-slate-500 flex-1"
       />
-      <button>Search</button>
+      <button className="bg-red-700 p-2 text-bold text-slate-100">Search</button>
     </form>
   );
 };
