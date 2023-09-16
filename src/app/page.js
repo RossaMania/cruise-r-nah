a5a87e208c19cf037a6b2947bdefe08e445f0864
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-16 16:54:33 -0500

    Add instructions to root page. Style. Import TMDB logo, and put on root page.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,32 @@
-import Searchbar from '@/components/Searchbar'
+import Searchbar from "@/components/Searchbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <Searchbar />
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <Searchbar />
+      <h1 className="text-4xl font-bold m-6 text-red-500">
+        Is it a Tom Cruise movie, or nah?
+      </h1>
+      <h2 className="text-2xl m-1">
+        Enter the title of a movie into the search bar, and click that Search
+        button to find out!
+      </h2>
+      <h2 className="text-xl m-2">
+        Or, you can check out the Tomography page featuring a comprehensive list
+        of Tom Cruise movies that Tom Cruise acted in or worked on behind the
+        scenes.
+      </h2>
+      <h2 className="text-xl m-2">
+        This product uses the TMDB API but is not endorsed or certified by TMDB.
+        We do not claim ownership of any of the images or data in the API.
+      </h2>
+      <Image
+        src={"blue_square_TMDBlogo.svg"}
+        alt="TMDB logo"
+        width={200}
+        height={50}
+      />
     </main>
-  )
+  );
 }
