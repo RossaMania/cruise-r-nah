53a415894551504730808c6ca3d78e31a141e26a
Ross Clettenberg <raclettenberg@gmail.com>
2023-09-16 20:25:31 -0500

    Adjust bg color of light theme to slate100.

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -7,7 +7,7 @@ import React from "react";
 const Providers = ({ children }) => {
   return (
     <ThemeProvider attribute="class">
-      <div className="dark:bg-gray-700 dark:text-red-200 text-gray-700 bg-slate-50 transition-colors duration-300 min-h-screen select-none">{children}</div>
+      <div className="dark:bg-gray-700 dark:text-red-200 text-gray-700 bg-slate-100 transition-colors duration-300 min-h-screen select-none">{children}</div>
     </ThemeProvider>
   );
 };
